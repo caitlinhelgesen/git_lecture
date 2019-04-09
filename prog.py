ee87407ab51b29b8dd6c59f9eb1aa80ed9a3823e
Caitlin Helgesen <chelgese@umich.edu>
2019-04-09 11:22:09 -0400

    second commit

--- prog.py
+++ prog.py
@@ -1,9 +1,13 @@
 import random
+import json
 
 
-def num_vowels(word):
-    letters = 0
-    for i in word:
-        if i in [a,e,i,o,u]:
-            letters += 1
-    return letters
+def num_vowels(word): #creates a function
+    vowels = ["a", "e", "i", "o", "u"]
+    letters = 0 #initializes counter at zero
+    for i in word: #for loop to iterate through word
+        if i in vowels: #find vowels
+            letters += 1 #count letters
+    return letters #return
+
+print(num_vowels("hello"))
